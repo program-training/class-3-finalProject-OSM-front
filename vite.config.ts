a8066ad68c18743e698243b48838b5331b977ed4
elchanan cohen <0548500232e@gmail.com>
2023-11-30 10:15:07 +0200

    tsconfig.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
-base: '/oms',
 plugins: [react()],
 test: {
 globals: true,
