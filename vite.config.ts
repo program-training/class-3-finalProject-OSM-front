9c763ff5b1ac74922727c037993d41cd62afdef0
unknown <rs315744656@gmail.com>
2023-11-30 12:39:41 +0200

    refael

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: '/oms',
-  plugins: [react()],
-});
-
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-// /// <reference types="vitest" />
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// // https://vitejs.dev/config/
-// export default defineConfig({
-// plugins: [react()],
-// test: {
-// globals: true,
-// environment: "jsdom",
-// setupFiles: "./src/tests/setup.ts",
-// },
-// });
-=======
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
@@ -39,4 +12,3 @@ setupFiles: "./src/tests/setup.ts",
     open: '/oms'
   }
 });
->>>>>>> 47b06e0c3f57853c17d933ba7856a3433e793b74
