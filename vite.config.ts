c944600c7667bbe5baf41b67fb1613aa4a6a67b0
unknown <rs315744656@gmail.com>
2023-12-04 10:12:21 +0200

    Change for an error in the build

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
+  command;
   return {
     plugins: [react()],
     base: env.VITE_BASE ? `/${env.VITE_BASE}/` : "/",
