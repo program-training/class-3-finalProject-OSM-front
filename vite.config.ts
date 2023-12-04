976e65add04b4486fded9830fcf538c587a91ec6
unknown <rs315744656@gmail.com>
2023-12-04 10:01:26 +0200

    to updeting vite.confing for devops

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,25 @@
 /// <reference types="vitest" />
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // base: "/env/",
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/tests/setup.ts",
-  },
-  server: {
-    open: "/oms",
-  },
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react()],
+    base: env.VITE_BASE ? `/${env.VITE_BASE}/` : "/",
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "./src/tests/setup.ts",
+    },
+    server: {
+      open: "/oms",
+      watch: {
+        usePolling: true,
+      },
+      host: true,
+      strictPort: true,
+      port: 3000,
+    },
+  };
 });
