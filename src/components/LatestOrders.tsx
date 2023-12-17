9c5f0d1fc6ff0bbbb87d3c4b8db0b2fed38d60dc
unknown <rs315744656@gmail.com>
2023-12-17 17:11:29 +0200

    Change in functions to match graphql

--- src/components/LatestOrders.tsx
+++ src/components/LatestOrders.tsx
@@ -7,13 +7,14 @@ import { requestGetOrders, requestDeleteOrder, requestPutOrderStatus } from "../
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { OrderDetailsDialog } from "./OrderDetailsDialog";
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { gql} from "@apollo/client";
+import { devLog } from "@apollo/client/invariantErrorCodes";
+// import { ApolloClient, InMemoryCache } from "@apollo/client";
+// import { gql} from "@apollo/client";
 
-const client = new ApolloClient({
-  uri: "http://localhost:8080/graphql",
-  cache: new InMemoryCache(),
-});
+// const client = new ApolloClient({
+//   uri: "http://localhost:8080/graphql",
+//   cache: new InMemoryCache(),
+// });
 
 const statusMap: { [key: string]: string } = {
   Pending: "#ffb84da9",
@@ -93,38 +94,20 @@ export function LatestOrders() {
     },
   ];
 
-useEffect(() => {
-  const fetchData = async () => {
-    const { data } = await client.query({
-      query: gql`
-        query GetAllOrders {
-          getAllOrders {
-            _id,
-            price,
-            shippingDetails{address},
-            shippingDetails{orderType},
-            status,
-            orderTime,
-            shippingDetails{userId}
-          }
-        }
-      ` 
-    });
-    console.log(data.getAllOrders);
-    setOrders(data.getAllOrders);
-  };
-
-  fetchData();
-}, []);
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const data = await requestGetOrders();
-      
-
-  //   };
-  //   fetchData();
-  // }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = (await requestGetOrders()).data;
+        console.log(data.getAllOrders);
+        setOrders(data.getAllOrders);
+      } catch (error) {
+        console.error("Error fetching orders:", error);
+      }
+    };
+  
+    fetchData();
+  }, []);
+  
 
   const handleRowClick = (params: GridCellParams) => {
     setSelectedOrder(params.row as CostumeOrders);
@@ -142,11 +125,17 @@ useEffect(() => {
   };
 
   const handleChangeStatus = async (orderId: string) => {
-    await requestPutOrderStatus(orderId);
-    const response = await requestGetOrders();
+   await requestPutOrderStatus(orderId);
+    const response = (await requestGetOrders()).data;
     showToastMessage();
-    setOrders(response);
-  };
+    setOrders(response.getAllOrders);
+    }
+    // await requestPutOrderStatus(orderId);
+    // const response = (await requestGetOrders()).data;
+    // showToastMessage();
+    // setOrders(response.getAllOrders);
+  // };
+ 
 
   const costumeOrders = orders
     ? orders.map((order: OrderInterface) => {
